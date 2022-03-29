74ef77bba6f47cd0bbefbe18db9b90ae96dc0cbf
hyejun <hyejun0608@naver.com>
2022-03-29 19:12:54 +0900

    [INIT]

---
+++ src/main/java/com/hyejun0608/BookstoreInMemoryBE/BookstoreInMemoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.hyejun0608.BookstoreInMemoryBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreInMemoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreInMemoryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyejun0608/BookstoreInMemoryBE/BookstoreInMemoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyejun0608.BookstoreInMemoryBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreInMemoryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
