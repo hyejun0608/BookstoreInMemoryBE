aa59a43fd121c797689da93e64d4662031d36f97
hyejun <hyejun0608@naver.com>
2022-05-16 10:11:54 +0900

    [ADD] UserDTO

---
+++ src/main/java/com/hyejun0608/BookstoreInMemoryBE/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.hyejun0608.BookstoreInMemoryBE.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserDTO {
+    private int userId;
+    private String id;
+    private String password;
+    private String name;
+}
