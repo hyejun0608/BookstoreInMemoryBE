26eb5c4916a9798e4050843c49efd5ddddceaa17
hyejun <hyejun0608@naver.com>
2022-05-17 14:50:25 +0900

    [ADD] JwtTokenProvider

---
+++ src/main/java/com/hyejun0608/BookstoreInMemoryBE/security/JwtTokenProvider.java
@@ -0,0 +1,12 @@
+package com.hyejun0608.BookstoreInMemoryBE.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+    private final
+}
