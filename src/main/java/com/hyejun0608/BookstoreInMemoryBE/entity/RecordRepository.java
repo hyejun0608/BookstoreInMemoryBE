240968520b947015a446650e991195d57abdaab6
hyejun <hyejun0608@naver.com>
2022-05-31 14:08:48 +0900

    [ADD] RecordRepository

---
+++ src/main/java/com/hyejun0608/BookstoreInMemoryBE/entity/RecordRepository.java
@@ -0,0 +1,6 @@
+package com.hyejun0608.BookstoreInMemoryBE.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<Record, Integer> {
+}
