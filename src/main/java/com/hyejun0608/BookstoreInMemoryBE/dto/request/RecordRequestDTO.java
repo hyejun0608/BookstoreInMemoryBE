49a267f6c7cd62add29188f0a2cbd8c9663dde29
hyejun <hyejun0608@naver.com>
2022-06-07 08:56:36 +0900

    [ADD] 어노테이션 추가

--- src/main/java/com/hyejun0608/BookstoreInMemoryBE/dto/request/RecordRequestDTO.java
+++ src/main/java/com/hyejun0608/BookstoreInMemoryBE/dto/request/RecordRequestDTO.java
@@ -5,18 +5,28 @@ package com.hyejun0608.BookstoreInMemoryBE.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Getter
 @Setter
 public class RecordRequestDTO {
+
     /* 제목 */
+    @NotBlank
     private String title;
 
     /* 저자 */
+    @NotBlank
     private String author;
 
     /* 내용 */
+    @NotBlank
     private String content;
 
     /* 읽은 횟수 */
+    @NotNull
+    @Positive
     private int count;
 }
